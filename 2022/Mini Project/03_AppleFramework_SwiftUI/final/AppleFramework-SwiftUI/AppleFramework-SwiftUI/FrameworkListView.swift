f556555ae201cfcae80efb9ee6e87f4cebc8168a
Jason Lee <jason@delightroom.com>
2022-08-21 15:45:35 +0900

    3차 파이널 업데이트

---
+++ 2022/Mini Project/03_AppleFramework_SwiftUI/final/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+struct FrameworkListView: View {
+    
+    @StateObject var vm = FrameworkListViewModel()
+    
+    let layout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    var body: some View {
+        
+        // Grid 만들기
+        // - UIKit : UICollectionView
+        //   - Data, Presentaion, Layout
+        // - SwiftUI : LazyVGrid, LazyHGrid
+        //   - ✅ Data, ✅ Presentaion, ✅ Layout
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: layout) {
+                    ForEach($vm.models) { $item in
+                        NavigationLink {
+                            let vm = FrameworkDetailViewModel(framework: item)
+                            FrameworkDetailView(viewModel: vm)
+                        } label: {
+                            FrameworkCell(framework: $item)
+                        }
+                    }
+                }
+                .padding([.top, .leading, .trailing], 16.0)
+            }
+            .navigationTitle("☀️ Apple Framework")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkListView()
+    }
+}
